4b423283b5c111ebee00a91992d2c71c1f11e462
prorokky <vlad6005@gmail.com>
2022-01-10 23:14:01 +0300

    feat(product): подключение MongoDB
    Подключение MongoDB к проекту

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useUnifiedTopology: true
+        }) // подключение к бд
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+
+}
+
+start()
