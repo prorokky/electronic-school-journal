06ae174e785a609e977496e42420aa5b2accedb0
prorokky <vlad6005@gmail.com>
2022-01-24 19:40:11 +0300

    feat(product): добавил верстку для создания пользователя

--- client/src/App/pages/AddUser/AddUser.tsx
+++ client/src/App/pages/AddUser/AddUser.tsx
@@ -1,14 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Button from '@components/Button'
+import Input from '@components/Input'
 import Navbar from '@components/Navbar'
 
 import styles from './AddUser.module.scss'
 
 const AddUser: React.FC = () => {
+	// TODO: добавить редакс, подставить нужные значения, поменять на массив инпуты
+	const [loginValue, setLoginValue] = useState<string>('')
+	const [passwordValue, setPasswordValue] = useState<string>('')
+
 	return (
 		<div className={styles.addUserContainer}>
 			<Navbar />
-			<h1>Add User Page</h1>
+			<div className={styles.formContainer}>
+				<form>
+					<h1 className={styles.formHead}>Добавление пользователя</h1>
+					<div className={styles.validateInput}>
+						<Input value={loginValue} placeholder={'Логин'} onChange={(event) => setLoginValue(event)} />
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.validateInput}>
+						<Input
+							value={passwordValue}
+							placeholder={'Пароль'}
+							onChange={(event) => setPasswordValue(event)}
+						/>
+					</div>
+					<div className={styles.createUser}>
+						<Button onClick={() => alert('Вы прожали кнопку')}>войти</Button>
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
