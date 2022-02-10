7c0ba373bb3317dff396d7bc5bd12cae2f97e57a
prorokky <vlad6005@gmail.com>
2022-02-11 01:00:50 +0300

    refactor(components): изменить логику компоненты auth

--- client/src/App/pages/Auth/Auth.tsx
+++ client/src/App/pages/Auth/Auth.tsx
@@ -1,34 +1,34 @@
-import React, { useContext, useEffect, useState } from 'react'
+// @ts-nocheck
+
+import React, { useContext } from 'react'
 
 import AuthImg from '@assets/auth-img.png'
 import Alert from '@components/Alert'
 import Button from '@components/Button'
 import Input from '@components/Input'
 import { AuthContext } from '@context/AuthContext'
-import { useHttp } from '@hooks/http.hook'
-import { cleanForm, onChangeLogin, onChangePassword } from '@store/auth/actions'
+import { clearErrors } from '@store/addContact/actions'
+import {AUTH_FAILED, AUTH_START, AUTH_SUCCESS, onChangeLogin, onChangePassword} from '@store/auth/actions'
 import { RootState } from '@store/rootReducer'
 import { useDispatch, useSelector } from 'react-redux'
 
+import { sentHttp } from '../../../helpers'
 import styles from './Auth.module.scss'
 
 const Auth: React.FC = () => {
-	const auth = useContext(AuthContext)
-	// @ts-ignore
-	const { loading, errors, request } = useHttp()
 	const login = useSelector((state: RootState) => state.authReducer.login)
 	const password = useSelector((state: RootState) => state.authReducer.password)
+	const isLoading = useSelector((state: RootState) => state.authReducer.isLoading)
+	const messages = useSelector((state: RootState) => state.authReducer.messages)
+	const auth = useContext(AuthContext)
+	const { request, errors } = sentHttp()
 	const dispatch = useDispatch()
-	const [message, setMessage] = useState<string>('')
-
-	useEffect(() => {
-		dispatch(cleanForm())
-		setMessage('')
-		setMessage(errors)
-	}, [errors])
 
 	const loginHandler = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
+		dispatch({
+			type: AUTH_START,
+		})
 		const payload = {
 			login,
 			password,
@@ -37,14 +37,30 @@ const Auth: React.FC = () => {
 			const data = await request('/api/auth/login', 'POST', { ...payload })
 			// @ts-ignore
 			auth.login(data.token, data.userId)
-		} catch (e) {}
-		dispatch(cleanForm())
+			dispatch({
+				type: AUTH_SUCCESS,
+			})
+		} catch (e) {
+			dispatch({
+				type: AUTH_FAILED,
+				payload: errors,
+			})
+		}
 	}
 
 	return (
 		<div className={styles.authContainer}>
 			<div className={styles.alertsContainer}>
-				{message && <Alert cleanError={() => setMessage('')} text={message} isWarning={true} />}
+				{messages?.map((message, index) => {
+					return (
+						<Alert
+							cleanError={() => dispatch(clearErrors())}
+							text={message.message}
+							key={index}
+							isWarning={message.isWarning}
+						/>
+					)
+				})}
 			</div>
 			<div className={styles.formContainer}>
 				<div className={styles.authIcon}>
@@ -68,7 +84,9 @@ const Auth: React.FC = () => {
 						/>
 					</div>
 					<div className={styles.signIn}>
-						<Button isDisabled={loading} color={'green'}>войти</Button>
+						<Button isDisabled={isLoading} color={'green'}>
+							войти
+						</Button>
 					</div>
 				</form>
 			</div>
--- client/src/store/auth/reducer.ts
+++ client/src/store/auth/reducer.ts
@@ -1,19 +1,34 @@
-import { CLEAN_FORM, ON_CHANGE_LOGIN, ON_CHANGE_PASSWORD } from './actions'
+import {AUTH, AUTH_FAILED, AUTH_START, AUTH_SUCCESS, CLEAR_ERRORS, ON_CHANGE_LOGIN, ON_CHANGE_PASSWORD} from './actions'
 import { AuthReducerState } from './types'
 
 const initialState: AuthReducerState = {
 	login: '',
 	password: '',
+	messages: [],
+	isLoading: false,
 }
 
 export const authReducer = (state = initialState, action: { type: any; payload: any }) => {
 	switch (action.type) {
+		case AUTH_START: {
+			return { ...state, messages: [], isLoading: true }
+		}
+		case AUTH_SUCCESS: {
+			return { login: '', password: '', isLoading: false }
+		}
+		case AUTH: {
+			return { ...state, isAuth: true }
+		}
+		case AUTH_FAILED: {
+			return { login: '', password: '', messages: action.payload, isLoading: false }
+		}
+		case CLEAR_ERRORS: {
+			return { ...state, messages: [] }
+		}
 		case ON_CHANGE_LOGIN:
 			return { ...state, login: action.payload }
 		case ON_CHANGE_PASSWORD:
 			return { ...state, password: action.payload }
-		case CLEAN_FORM:
-			return { login: '', password: '' }
 		default:
 			return state
 	}
